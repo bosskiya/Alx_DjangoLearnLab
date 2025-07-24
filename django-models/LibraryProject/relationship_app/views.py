2979f0e61977edd40d2e2beac37b37d6745b6561
binikiya <biniyamkiya3682@gmail.com>
2025-07-24 10:31:18 +0300

    Django Models

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 
 class LibraryDetailView(DetailView):
