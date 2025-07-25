429e48b60f877b33105c6870e5f15ca9f6900d85
binikiya <biniyamkiya3682@gmail.com>
2025-07-25 12:44:39 +0300

    Advanced Features and Security

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Book
-from .forms import BookForm, ExampleForm
+from .forms import BookForm
+from .forms import ExampleForm
 from django.db.models import Q
 
 @permission_required('bookshelf.can_view', raise_exception=True)
