0df5663678996b608c40a79fb3df6fc9a08583db
binikiya <biniyamkiya3682@gmail.com>
2025-08-08 12:32:59 +0300

    API Project

--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -3,9 +3,6 @@ from .models import Book
 
 
 class BookSerializer(serializers.ModelSerializer):
-    created = serializers.DateTimeField(read_only=True)
-    updated = serializers.DateTimeField(read_only=True)
-
     class Meta:
         model = Book
-        fields = ['id', 'title', 'author']
+        fields = '__all__'
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,13 +1,19 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework import status
 from .models import Book
 from .serializers import BookSerializer
 
 
 class BookList(generics.ListAPIView):
-    http_method_names = ('patch', 'get')
     permission_classes = [permissions.AllowAny,]
     serializer_class = BookSerializer
 
     def get_queryset(self):
         return Book.objects.all()
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
