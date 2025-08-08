1172691883a32a5703b1de99d8d5324daabe76b9
binikiya <biniyamkiya3682@gmail.com>
2025-08-08 12:08:10 +0300

    API Project

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,10 +1,10 @@
-from rest_framework.generics import ListAPIView
+from rest_framework import generics
 from rest_framework import permissions
 from .models import Book
 from .serializers import BookSerializer
 
 
-class BookList(ListAPIView):
+class BookList(generics.ListAPIView):
     http_method_names = ('patch', 'get')
     permission_classes = [permissions.AllowAny,]
     serializer_class = BookSerializer
