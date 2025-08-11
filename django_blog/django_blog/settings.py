7983df8314b9cbdc538549fab53676c044a3f010
binikiya <biniyamkiya3682@gmail.com>
2025-08-11 21:22:19 +0300

    Django Blog

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'postgres',
+        'PORT': 5432,
+        'PASSWORD': 'bini',
+        'HOST': 'localhost',
     }
 }
 
