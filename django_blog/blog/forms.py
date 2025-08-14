e70eb1344169c67ffe099c30e1b418060ea37fe9
binikiya <biniyamkiya3682@gmail.com>
2025-08-14 23:14:29 +0300

    Django Blog

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -19,7 +19,7 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ['title', 'content', 'tags']
         widgets = {
-            'tags': TagWidget(attrs={'placeholder': 'Add tags separated by commas'}),
+            'tags': TagWidget(),
         }
 
 
