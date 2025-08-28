cc1702ba74e3a63d8417c2769b0403c1927465aa
binikiya <biniyamkiya3682@gmail.com>
2025-08-28 22:59:18 +0300

    Social Media API

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -91,8 +91,11 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'social_media_app',
+        'USER': 'postgres',
+        'PASSWORD': 'bini',
+        'HOST': 'localhost',
     }
 }
 
